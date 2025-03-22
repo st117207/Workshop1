3675c47162c911f69b47ae833a537d45a4ed82f9
st117207 <st117207@student.spbu.ru>
2025-03-22 18:21:36 +0300

    Код обновлён

--- Precise measure/Precise measure/Source.cpp
+++ Precise measure/Precise measure/Source.cpp
@@ -2,45 +2,97 @@
 #include <fstream>
 #include <cmath>
 #include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
 
+
+void inputdata(double arr[], double n)
+{
+	std::ifstream inputFile("Precise measurements.csv");
+	for (int i = 0; i < n; i++) {
+		inputFile >> arr[i];
+	}
+}
+double arithmetic_mean(double arr[], double n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+	}
+	double arithmetic = 0.0;
+	arithmetic = static_cast<double>(round(sum / n * 1000)) / 1000;
+	return arithmetic;
+}
+double relative_error(double arr[], double n)
+{
+	double fx = arithmetic_mean(arr, n);
+	double T = 1;
+	double rerror = (5 * pow(10, -7.0) + 1 / (fx * 1000 * T)) * 100;
+	return rerror;
+}
+double absolute_error(double arr[], double n)
+{
+	double rerror = relative_error(arr, n);
+	double fx = arithmetic_mean(arr, n);
+	double aerror = rerror * fx / 100;
+	return aerror;
+}
+void RandomDeviations(double arr1[], double arr2[], double arr3[], double n)
+{
+	double arithmetic = arithmetic_mean(arr1, n);
+	for (int i = 0; i < n; i++) {
+		arr2[i] = arr1[i] - arithmetic;
+		arr3[i] = arr2[i] * arr2[i];
+	}
+}
+float dispersion(double arr3[], double n)
+{
+	float sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr3[i];
+	}
+	return sqrt(sum / (n - 1));
+}
+double root_mean_square_error_mean(double arr3[], double n)
+{
+	return dispersion(arr3, n) / sqrt(n);
+}
+double total_error(double arr1[], double arr3[], double n)
+{
+	return sqrt(pow(arithmetic_mean(arr1, n) / 3, 2) + pow(root_mean_square_error_mean(arr3, n), 2));
+}
+void outputdata(double arr1[], double arr2[], double arr3[], double n)
+{
+	float sum1 = 0;
+	float sum2 = 0;
+	std::ofstream outputFile("Precise Result.csv");
+	RandomDeviations(arr1, arr2, arr3, n);
+	outputFile << arithmetic_mean(arr1, n) << std::endl;
+	outputFile << std::setprecision(4) << absolute_error(arr1, n) << std::endl;
+	outputFile << std::setprecision(4) << dispersion(arr3, n) << std::endl;
+	outputFile << std::setprecision(4) << root_mean_square_error_mean(arr3, n) << std::endl;
+	outputFile << std::setprecision(4) << total_error(arr1, arr3, n) << std::endl;
+	outputFile << std::endl;
+	for (int i = 0; i < n; i++) {
+		outputFile << arr2[i] << " " << arr3[i] << std::endl;
+		sum1 += arr2[i];
+		sum2 += arr3[i];
+	}
+	outputFile << "----------------------"<<std::endl;
+	outputFile << std::setprecision(3) << sum1 << " " << std::setprecision(6) << sum2;
+
+
+
+}
 int main() {
-    std::ifstream inputFile("Precise measurements.csv");
-    std::ofstream outputFile("Precise Result.csv");
-    float n = 50;
-    float sum = 0.0;
-    double temp = 0;
-    double data[50];
-    double value;
-    //Считывание данных
-    for (int i = 0; i < n; i++) {
-        inputFile >> value;
-        data[i] = value;
-        sum += value;
-    }
+	int n = 43;
+	double data1[43];
+	double data2[43];
+	double data3[43];
+	inputdata(data1, n);
+	outputdata(data1, data2, data3, n);
 
-
-    inputFile.close();
-    //Среднее арифметическое
-    float fx =round(sum / n * 1000) / 1000;
-    float T = 1;
-    //Относительная погрешность
-    float rerror = (5 * pow(10, -7.0) + 1 / (fx * 1000 * T)) * 100;
-    //Абсолютная погрешность
-    float aerror = rerror * fx * 1000 / 100;
-    //Дисперсия
-    for (int i = 0; i < n; i++) {
-        temp += pow((data[i] - fx), 2);
-    }
-    double sygma = sqrt(temp / (n - 1));
-    //Средняя квадратичная погрешность среднего
-    double sygmax = sygma / sqrt(n);
-    outputFile <<fx << std::endl;
-    outputFile << std::fixed<<std::setprecision(6) << aerror / 1000 << std::endl;
-    outputFile << sygma << std::endl;
-    outputFile << sygmax << std::endl;
-
-
-    outputFile.close();
-
-    return 0;
+	return 0;
 }
--- Rough measure/Rough measure/Source.cpp
+++ Rough measure/Rough measure/Source.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <iomanip>
 
+
+void inputdata(float arr[], double n)
+{
+	std::ifstream inputFile("Rough measurements.csv");
+	for (int i = 0; i < n; i++) {
+		inputFile >> arr[i];
+	}
+}
+float arithmetic_mean(float arr[], double n)
+{
+	float sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+	}
+	return sum / n;
+}
+float relative_error(float arr[], double n)
+{
+	float fx = arithmetic_mean(arr, n);
+	float T = 0.1;
+	float rerror = (5 * pow(10, -7.0) + 1 / (fx * 1000 * T)) * 100;
+	return rerror;
+}
+float absolute_error(float arr[], double n)
+{
+	float rerror = relative_error(arr, n);
+	float fx = arithmetic_mean(arr, n);
+	float aerror = rerror * fx / 100;
+	return aerror;
+}
+void outputdata(float arr[], double n)
+{
+	std::ofstream outputFile("Rough Result.csv");
+	outputFile << arithmetic_mean(arr, n)<<std::endl;
+	outputFile << std::setprecision(4) << absolute_error(arr, n) << std::endl;
+
+}
 int main() {
-    std::ifstream inputFile("Rough measurements.csv");
-    std::ofstream outputFile("Rough Result.csv");
-    float n = 10;
-    float sum = 0.0;
-    //Считывание данных
-    double value;
-    for (int i = 0; i < n; i++) {
-        inputFile >> value;
-            sum += value;
-    }
+	int n = 10;
+	float data[10];
+	inputdata(data, n);
+	outputdata(data, n);
 
-
-    inputFile.close();
-    //Среднее арифмитическое
-    float fx = sum / n;
-    float T = 0.1;
-    //Относительная погрешность
-    float rerror = (5 * pow(10, -7.0) + 1 / (fx*1000 * T)) * 100;
-    //Абсолютная погрешность
-    float aerror = rerror * fx*1000 / 100;
-    outputFile <<  fx << std::endl;
-    outputFile << aerror/1000 << std::endl;
-    outputFile.close();
-
-    return 0;
+	return 0;
 }
